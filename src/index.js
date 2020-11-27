49776bb318bc54ebcda2bd4a151ebfddaa4438cf
Gi-Isler <giselaisler25@gmail.com>
2020-11-26 21:24:58 -0300

    cambio de pc

--- src/index.js
+++ src/index.js
@@ -13,26 +13,39 @@ document.getElementById("validar").addEventListener('click', function(){        
          while (x>=0){                                                     //verifica si se cumple condicion
          numerotarjetainvertida += numerotarjeta.charAt(x);                //envia ultima posicion del elemento a numerotarjetainvertida
             x--;
-         parseInt(numerotarjetainvertida.trim).value;                      //Variable se toma valor se numero y  se eliminan espacios vacios dentro de ella
+         parseInt(numerotarjetainvertida.trim).value;                      //Variable se convierte en numero y  se eliminan espacios vacios dentro de ella
          }
 let par = [];
 let impar= [];
                for ( let i=0; i< numerotarjetainvertida.length ;i++ ) {                    //recorriendo el array
                   if (i%2===0){                                                           //pregunta si la posicion es par
-                     par.push(numerotarjetainvertida[i]*2);                               //posicion par multiplicada
+                     par.push(numerotarjetainvertida[i]*2);                               //posicion par multiplica y envia a variable 
                       for (let j in par){
                            if (par [j]>9){                                                //identifica resultado con valor sobre 9
                               par [j]= (parseInt(par[j]%10)) + (parseInt(par[j]/10));     //reduce valor a 1 digito suma
                            }
-                        }
+                      }
 
                   } else {
-                     impar.push(numerotarjetainvertida[i]);                                //posicion impar envia a su variable
+                     impar.push(numerotarjetainvertida[i]);                                //posicion impar enviada a su variable
                     }
-let sum= par.concat(impar);
+let sum= par.concat(impar);                                    //une los array, en una nueva
 parseInt(sum).value;
-console.log(sum);
-// let suma = sum.reduce( (a,b) => a + b,);
-               }
+
+let suma = sum.reduce((a, b) => a + b, 0);[];                  //suma elementos del array
+parseInt(suma).value;
+    if (suma % 10 === 0 && suma!==0){
+         // const respuesta = document.getElementById('respuesta');
+  if (result % 10 === 0) {
+   //  respuesta.innerHTML = `
+         // return alert('Tarjeta válida');
+         
+      //  }
+         // else{
+         // return alert('Tarjeta inválida');
+      //   }
+// console.log(suma);
+//   }
+//   document.getElementById('formulariotarjeta').reset();
 }
 })
